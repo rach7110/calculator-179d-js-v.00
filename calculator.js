7ded309932f71fce0092feb0dbe8e4ad73c531c1
Rachel Loziuk <rachel.loziuk@gmail.com>
2014-02-17 15:10:13 -0600

    moved submit button

--- calculator.js
+++ calculator.js
@@ -1,8 +1,6 @@
 // window.onload = (function(){
 //   alert("Works!");
 // });
-
-
 $(document).ready(function(){
 	event.preventDefault();
   // alert("Works so far!");
@@ -11,14 +9,15 @@ $(document).ready(function(){
 		var size = document.getElementById("size").value;
     var name = document.getElementById("name").value;
     var lighting = document.getElementById("lighting").value;
-    // alert("Great!");
-
+    // Checks the user entered a building size:
     if(size =="") {
       alert("Please enter a building size");
       return;
+    //Checks the user entered a building size greater than or equal to 100,000 sqft: 
     } else if(parseInt(size) < 100000) {
         alert("Building size must be larger than 100,000 sqft");
         return;
+    //Checks the user entered a name: 
     } else if(name.length == 0) {
         alert("Please eneter your name");
         return;
@@ -27,16 +26,4 @@ $(document).ready(function(){
         alert("Thanks, " + name + ". You submitted: " + size + " sqft.");
     };
   });
-    // else if(size == "") {
-    //    alert("Please enter a building size");
-    // }
-    // if(name.length == 0) {
-    //   alert("Please enter your name.");
-    //   return;
-    // }
-    // if{
-    //   var size = parseInt(size)
-    //   alert(typeof size)
-    //   alert("Thanks, " + name + ". You submitted: " + size + " sqft.");
-    // }
 });
