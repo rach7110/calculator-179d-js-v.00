128e18670de8ff439349bc0c59a339decf860a18
Rachel Loziuk <rachel.loziuk@gmail.com>
2014-02-17 14:35:30 -0600

    sqitched order of error checks with name and buidling size; added a User Journey file

--- calculator.js
+++ calculator.js
@@ -12,20 +12,31 @@ $(document).ready(function(){
     var name = document.getElementById("name").value;
     var lighting = document.getElementById("lighting").value;
     // alert("Great!");
-    if(name.length == 0) {
-      alert("Please enter your name.");
+
+    if(size =="") {
+      alert("Please enter a building size");
       return;
-    }
-    else if(parseInt(size) < 100000) {
-      alert("Building size must be larger than 100,000 sqft");
-    }
-    else if(size == "") {
-       alert("Please enter a building size");
-    }
-    else {
-      var size = parseInt(size)
-      alert(typeof size)
-      alert("Thanks, " + name + ". You submitted: " + size + " sqft.");
-    }
-	});
+    } else if(parseInt(size) < 100000) {
+        alert("Building size must be larger than 100,000 sqft");
+        return;
+    } else if(name.length == 0) {
+        alert("Please eneter your name");
+        return;
+    } else {
+        var size = parseInt(size);
+        alert("Thanks, " + name + ". You submitted: " + size + " sqft.");
+    };
+  });
+    // else if(size == "") {
+    //    alert("Please enter a building size");
+    // }
+    // if(name.length == 0) {
+    //   alert("Please enter your name.");
+    //   return;
+    // }
+    // if{
+    //   var size = parseInt(size)
+    //   alert(typeof size)
+    //   alert("Thanks, " + name + ". You submitted: " + size + " sqft.");
+    // }
 });
