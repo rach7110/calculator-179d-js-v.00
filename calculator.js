3564c97aa1fac44e8f3e98a5fe5dca77691dc7ca
Rachel Loziuk <rachel.loziuk@gmail.com>
2014-02-17 16:02:38 -0600

    confirms whether any of the 3 efficiencies are selected

--- calculator.js
+++ calculator.js
@@ -8,7 +8,10 @@ $(document).ready(function(){
   // alert("Works so far!");
 		var size = document.getElementById("size").value;
     var name = document.getElementById("name").value;
-    var lighting = document.getElementById("lighting").value;
+    var lightingLength = $('input#lighting:checked').length;
+    var hvacLength = $('input#hvac:checked').length;
+    var envLength = $('input#env:checked').length;
+
     // Checks the user entered a building size:
     if(size =="") {
       alert("Please enter a building size");
@@ -20,10 +23,32 @@ $(document).ready(function(){
     //Checks the user entered a name: 
     } else if(name.length == 0) {
         alert("Please eneter your name");
-        return;
+        return;     
+    // } else {
+    //     var size = parseInt(size);
+    //     alert("Thanks, " + name + ". You submitted: " + size + " sqft.");
+    };
+
+// Checks if the lighting checkbox is selected:
+    // alert("('input#lighting:checked').length is:  " +  lightingLength);
+    if (lightingLength > 0) {
+      alert("Lighting is checked!!");
     } else {
-        var size = parseInt(size);
-        alert("Thanks, " + name + ". You submitted: " + size + " sqft.");
+      alert('Lighting is NOT checked');
+    }
+// Checks if the HVAC checkbox is selected:
+    // alert("('input#hvac:checked').length is:  " +  hvacLength);
+    if (hvacLength > 0) {
+      alert("HVAC is checked!!");
+    } else {
+      alert('HVAC is NOT checked');
+    };
+// Checks if the envelope checkbox is selected:
+    // alert("('input#env:checked').length is:  " +  envLength);
+    if(envLength > 0) {
+      alert("Envelope is checked!!");
+    } else {
+      alert('Envelope is NOT checked');
     };
   });
 });
