aea09d4d62de8a43a88f75377884bd92416905ec
Rachel Loziuk <rachel.loziuk@gmail.com>
2014-02-19 11:07:02 -0600

    adjusted configuration settings to tell Sinatra root of app

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,11 @@
 # require 'Bundler/setup'
 # Bundler.require
 
+# sets root as the parent-directory of the current file
+set :root, File.join(File.dirname(__FILE__), '..')
+# sets the view directory correctly
+set :views, Proc.new { File.join(root, "views") } 
+
 require 'sinatra'
 
 get '/' do
