50aa8717116589719b860d8976c71b1a0c33bb9d
Rachel Loziuk <rachel.loziuk@gmail.com>
2014-02-19 11:17:20 -0600

    moved sinatra to top of .rb file

--- calculator.rb
+++ calculator.rb
@@ -1,12 +1,12 @@
 # require 'Bundler/setup'
 # Bundler.require
-
+require 'sinatra'
 # sets root as the parent-directory of the current file
 set :root, File.join(File.dirname(__FILE__), '..')
 # sets the view directory correctly
 set :views, Proc.new { File.join(root, "views") } 
 
-require 'sinatra'
+
 
 get '/' do
   erb :test
