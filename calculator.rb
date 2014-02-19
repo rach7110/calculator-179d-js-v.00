4047432d96e39d8b9c8a60a36f4cb13a9d1c8883
Rachel Loziuk <rachel.loziuk@gmail.com>
2014-02-19 11:24:03 -0600

    removed the settings which set the root directory

--- calculator.rb
+++ calculator.rb
@@ -1,12 +1,11 @@
 # require 'Bundler/setup'
 # Bundler.require
 require 'sinatra'
-# sets root as the parent-directory of the current file
-set :root, File.join(File.dirname(__FILE__), '..')
-# sets the view directory correctly
-set :views, Proc.new { File.join(root, "views") } 
 
-
+# # sets root as the parent-directory of the current file
+# set :root, File.join(File.dirname(__FILE__), '..')
+# # sets the view directory correctly
+# set :views, Proc.new { File.join(root, "views") } 
 
 get '/' do
   erb :test
