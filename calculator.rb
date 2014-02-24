0440b7898d396182b0879d430d54cb5041197d7b
Rachel Loziuk <rachel.loziuk@gmail.com>
2014-02-24 15:01:43 -0600

    set the views folder correctly; changed some styling and html selectors

--- calculator.rb
+++ calculator.rb
@@ -2,13 +2,13 @@
 # Bundler.require
 require 'sinatra'
 
-# # sets root as the parent-directory of the current file
-# set :root, File.join(File.dirname(__FILE__), '..')
-# # sets the view directory correctly
-# set :views, Proc.new { File.join(root, "views") } 
+# sets root as the parent-directory of the current file
+set :root, File.join(File.dirname(__FILE__), '..')
+# sets the view directory correctly
+set :views, Proc.new { File.join(root, "Views") } 
 
 get '/' do
-  erb :test
+  erb :index
 end
 
 
