6db5ba31f3711ff75e6d4f6dfbe845812291f6d8
Rachel Loziuk <rachel.loziuk@gmail.com>
2014-02-24 15:17:59 -0600

    removed set views - realy this time

--- calculator.rb
+++ calculator.rb
@@ -2,11 +2,6 @@
 # Bundler.require
 require 'sinatra'
 
-# # sets root as the parent-directory of the current file
-# set :root, File.join(File.dirname(__FILE__), '..')
-# # sets the view directory correctly
-# set :views, Proc.new { File.join(root, "Views") } 
-
 get '/' do
   erb :index
 end
